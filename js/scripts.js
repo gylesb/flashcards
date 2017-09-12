086068951658dd5647ca4740ba4b4653dcb2eeff
Gyles Batara and Hansen Hansen <hansen@>
2017-09-12 11:05:47 -0700

    Add JS scripts to make definitions toggle

---
+++ js/scripts.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+  $("li#js").click(function() {
+    $("#jsDef").toggle();
+  });
+
+  $("li#op").click(function() {
+    $("#opDef").toggle();
+  });
+
+  $("li#var1").click(function() {
+    $("#var1Def").toggle();
+  });
+
+  $("li#var2").click(function() {
+    $("#var2Def").toggle();
+  });
+
+  $("li#func").click(function() {
+    $("#funcDef").toggle();
+  });
+
+  $("li#meth").click(function() {
+    $("#methDef").toggle();
+  });
+
+  $("li#arg").click(function() {
+    $("#argDef").toggle();
+  });
+
+  $("li#par").click(function() {
+    $("#parDef").toggle();
+  });
+
+  $("li#ret").click(function() {
+    $("#retDef").toggle();
+  });
+
+  $("li#chain").click(function() {
+    $("#chainDef").toggle();
+  });
+
+  $("li#str").click(function() {
+    $("#strDef").toggle();
+  });
+
+  $("li#boo").click(function() {
+    $("#booDef").toggle();
+  });
+
+  $("li#und").click(function() {
+    $("#undDef").toggle();
+  });
+
+  $("li#nan").click(function() {
+    $("#nanDef").toggle();
+  });
+
+  $("li#esc").click(function() {
+    $("#escDef").toggle();
+  });
+
+  $("li#alert").click(function() {
+    $("#alertDef").toggle();
+  });
+
+  $("li#cmd").click(function() {
+    $("#cmdDef").toggle();
+  });
+
+  $("li#jq").click(function() {
+    $("#jqDef").toggle();
+  });
+
+  $("li#atr").click(function() {
+    $("#atrDef").toggle();
+  });
+});
